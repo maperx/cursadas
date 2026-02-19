ad530ae737d6da3e9397e8bcbf5f3ac4fb82f235
maperx <maperx@gmail.com>
2026-02-19 13:48:46 -0300

    arreglo boton Nueva Asignatura

---
+++ app/(admin)/admin/asignaturas/loading.tsx
@@ -0,0 +1,23 @@
+import { Skeleton } from "@/components/ui/skeleton";
+
+export default function AsignaturasLoading() {
+  return (
+    <div className="space-y-6">
+      <div className="flex items-center justify-between">
+        <div className="space-y-2">
+          <Skeleton className="h-9 w-40" />
+          <Skeleton className="h-5 w-64" />
+        </div>
+        <Skeleton className="h-9 w-44" />
+      </div>
+      <Skeleton className="h-9 w-64" />
+      <div className="rounded-md border">
+        <div className="p-4 space-y-4">
+          {Array.from({ length: 5 }).map((_, i) => (
+            <Skeleton key={i} className="h-12 w-full" />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
