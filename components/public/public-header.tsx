0d65f8be44d0cf08b81a86831dd962457a65637c
maperx <maperx@gmail.com>
2026-02-23 14:12:51 -0300

    full width

--- components/public/public-header.tsx
+++ components/public/public-header.tsx
@@ -26,7 +26,7 @@ export function PublicHeader() {
 
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-backdrop-filter:bg-background/60 m-2">
-      <div className="container flex h-14 items-center justify-between">
+      <div className="flex h-14 items-center justify-between px-4 sm:px-6 lg:px-8">
         <Link href="/" className="flex items-center gap-2 font-semibold">
           <Image
             src="/logo.png"
